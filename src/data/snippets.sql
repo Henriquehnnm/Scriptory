6fe280f2a43e6fed08cd6c411d5d98931e6a9c1d
Henrique <hello.henrique@proton.me>
2026-03-29 08:29:58 -0300

    Add TS Dev

--- src/data/snippets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS snippets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    language TEXT,
-    content TEXT,
-    description TEXT,
-    created_at DATETIME,
-    tags TEXT
-)
